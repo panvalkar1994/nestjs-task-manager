f8912a344b3439f04bf940a68bbf082e36c6c18e
vaibhav <vpanvalkar@outlook.com>
2021-11-28 20:38:48 +0530

    basic signup

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,28 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "./user.entity";
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User>{
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({
+      username,
+      password,
+    });
+    try {
+        await this.save(user);
+    } catch (error) {
+        if(error.code===UserError.DUPLICATE_USERNAME){
+            throw new ConflictException('User Name taken already');
+        }else{
+            throw new InternalServerErrorException();
+        }
+    }
+  }
+}
 
+export enum UserError{
+    DUPLICATE_USERNAME='23505',
 }
