484f061801fd62ce25c5a67fa7ef4cbf6e262ac3
vaibhav <vpanvalkar@outlook.com>
2021-12-05 19:08:03 +0530

    jwt integrated

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "./dto/jwt-payload.interface";
+import { User } from "./user.entity";
+import { UsersRepository } from "./users.repository";
+
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(
+        @InjectRepository(UsersRepository)
+        private userRepository:UsersRepository
+    ){
+        super({
+            jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration:false,
+            secretOrKey:"abc123"            
+        });
+    }
+
+    async validate(payload:JwtPayload) : Promise<User>{
+        const {username} = payload;
+        const user:User = await this.userRepository.findOne({username});
+        if(!user){
+            throw new UnauthorizedException()
+        }
+
+        return user;
+    }
+}
