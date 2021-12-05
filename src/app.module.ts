304245ed86df92b9975ba6712b66a5d766cf994e
vaibhav <vpanvalkar@outlook.com>
2021-12-05 14:17:09 +0530

    auth clean slate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,7 +15,6 @@ import { AuthModule } from './auth/auth.module';
       autoLoadEntities:true,
       synchronize:true,
     }),
-    AuthModule
   ],
 })
 export class AppModule {}
